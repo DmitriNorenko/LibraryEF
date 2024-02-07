a5373dd2813765c79f440d3bdf97813a088552d6
Name <email@exaple.ru>
2024-02-07 10:59:54 +0300

    Update ConnectionString

--- LibraryEF/AppContext.cs
+++ LibraryEF/AppContext.cs
@@ -18,7 +18,7 @@ namespace LibraryEF
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=DESKTOP-SFAH3FP\SQLEXPRESS;
-            DataBase=EF;Trusted_Connection = True; TrustServerCertificate = True");
+            DataBase=LibraryEF;Trusted_Connection = True; TrustServerCertificate = True");
         }
     }
 }
