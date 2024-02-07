d681da263b6310ba7f8264266e65aa0f1985f024
Name <email@exaple.ru>
2024-02-07 10:58:09 +0300

    Update AppContext

--- LibraryEF/AppContext.cs
+++ LibraryEF/AppContext.cs
@@ -9,7 +9,12 @@ namespace LibraryEF
 {
     public class AppContext : DbContext
     {
-
+        DbSet<User> Users { get; set; }
+        DbSet<Book> Books { get; set; }
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=DESKTOP-SFAH3FP\SQLEXPRESS;
