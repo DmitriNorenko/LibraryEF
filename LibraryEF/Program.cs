6a7e2ec59d7d04230c437244ff6947d4fc873e5b
Name <email@exaple.ru>
2024-02-07 13:21:11 +0300

    Add UpdateUserName

--- LibraryEF/Program.cs
+++ LibraryEF/Program.cs
@@ -9,10 +9,12 @@ namespace LibraryEF
            using(var db = new AppContext())
             {
                 UserRepository userRepository = new UserRepository(db.Users);
-                //userRepository.AddUser();
+                userRepository.AddUser();
                 userRepository.ShowAll();
                 userRepository.DeleteUser();
                 userRepository.ShowAll();
+                userRepository.ShowUser();
+                userRepository.UpdateUserName();
                 userRepository.ShowUser();
                 db.SaveChanges();
             }
--- LibraryEF/UserRepository.cs
+++ LibraryEF/UserRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
@@ -60,6 +61,23 @@ namespace LibraryEF
                 {
                     Console.WriteLine("Пользователь найден!");
                     Console.WriteLine($"{user.Id}\t{user.Name}\t{user.Email}");
+                    return;
+                }
+            }
+            Console.WriteLine("Такого пользователя нет.");
+        }
+        public void UpdateUserName()
+        {
+            Console.WriteLine("\nВведите id пользователя для изменения: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            foreach (var user in Users)
+            {
+                if (user.Id == id)
+                {
+                    Console.WriteLine("Пользователь найден!");
+                    Console.WriteLine("Введите новое имя:");
+                    user.Name = Console.ReadLine();
+                    Console.WriteLine("Изменения сохранены!");
                     return;
                 }
             }
